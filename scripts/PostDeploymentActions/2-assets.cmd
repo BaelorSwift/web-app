106ba2bb7c0e799e79d909d45bcc2c00f0e436ad
Alex Forbes-Reed <alexreed@outlook.com>
2015-02-26 01:29:53 +0000

    attempt 2

--- scripts/PostDeploymentActions/2-assets.cmd
+++ scripts/PostDeploymentActions/2-assets.cmd
@@ -3,6 +3,4 @@
 
 call cd "D:\home\site\approot\src\BaelorApi\"
 call npm install
-call cmd.exe /c grunt -b "D:\home\site\approot\src\BaelorApi\" --gruntfile "D:\home\site\approot\src\BaelorApi\gruntfile.js" bower
-call cmd.exe /c grunt -b "D:\home\site\approot\src\BaelorApi\" --gruntfile "D:\home\site\approot\src\BaelorApi\gruntfile.js" sass
-call cmd.exe /c grunt -b "D:\home\site\approot\src\BaelorApi\" --gruntfile "D:\home\site\approot\src\BaelorApi\gruntfile.js" typescript
+call grunt default
