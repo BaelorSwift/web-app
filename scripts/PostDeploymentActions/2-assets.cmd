12dea8ebcd151d8ae3f9d310ac2017ff9007e52a
Alex Forbes-Reed <alexreed@outlook.com>
2015-02-26 01:24:35 +0000

    updated asset post-deploy script

--- scripts/PostDeploymentActions/2-assets.cmd
+++ scripts/PostDeploymentActions/2-assets.cmd
@@ -3,6 +3,6 @@
 
 call cd "D:\home\site\approot\src\BaelorApi\"
 call npm install
-call grunt bower:install
-call grunt sass
-call grunt typescript
+call cmd.exe /c grunt -b "D:\home\site\approot\src\BaelorApi\" --gruntfile "D:\home\site\approot\src\BaelorApi\gruntfile.js" bower
+call cmd.exe /c grunt -b "D:\home\site\approot\src\BaelorApi\" --gruntfile "D:\home\site\approot\src\BaelorApi\gruntfile.js" sass
+call cmd.exe /c grunt -b "D:\home\site\approot\src\BaelorApi\" --gruntfile "D:\home\site\approot\src\BaelorApi\gruntfile.js" typescript
