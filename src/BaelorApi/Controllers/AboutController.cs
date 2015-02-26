7fc9c89cc2ebc750bc8f71a10315ac16a58dc384
Alex Forbes-Reed <alexreed@outlook.com>
2015-02-26 02:59:43 +0000

    created docs/about controller/view placeholders, tweaked styling

---
+++ src/BaelorApi/Controllers/AboutController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.Mvc;
+
+namespace BaelorApi.Controllers
+{
+	public class AboutController : Controller
+	{
+		// GET: /<controller>/
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
---
+++ src/BaelorApi/Controllers/DocsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.Mvc;
+
+namespace BaelorApi.Controllers
+{
+	public class DocsController : Controller
+	{
+		// GET: /<controller>/
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
