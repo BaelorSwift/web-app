cabfa0cabd8fc911846195d3d9b57b0972f96a14
Alex Forbes-Reed <github@alx.red>
2017-01-11 20:08:29 +0000

    Created baelor error struct

---
+++ models/baelor_error.go
@@ -0,0 +1,20 @@
+package models
+
+import "github.com/howeyc/crc16"
+
+// BaelorError contains error information to be picked up by the custom error
+// middleware for gin
+type BaelorError struct {
+	Code     string              `json:"code"`
+	Status   uint16              `json:"status"`
+	Metadata map[string][]string `json:"metadata"`
+}
+
+// NewBaelorError creates a new BaelorError based on the inputs
+func NewBaelorError(code string, metadata map[string][]string) BaelorError {
+	return BaelorError{
+		Code:     code,
+		Status:   crc16.ChecksumCCITT([]byte(code)),
+		Metadata: metadata,
+	}
+}
