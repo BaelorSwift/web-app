b50335d3f4f5efe49e2311368ed3a1ff364c4867
Alex Forbes-Reed <github@alx.red>
2017-01-11 20:08:44 +0000

    fixed json schema validator helper

--- helpers/validator.go
+++ helpers/validator.go
@@ -1,15 +1,16 @@
 package helpers
 
-import goSchema "github.com/xeipuuv/gojsonschema"
-import "fmt"
-import "os"
+import (
+	"fmt"
+
+	goSchema "github.com/xeipuuv/gojsonschema"
+)
 
 // Validate an object based on a json schema
-func Validate(obj interface{}, schemaName string) (bool, []goSchema.ResultError) {
-	fmt.Println(os.Args[0])
-
-	schema := goSchema.NewReferenceLoader("schema/albums.json") //fmt.Sprintf("schema/%s.json", schemaName))
-	doc := goSchema.NewGoLoader(&obj)
+func Validate(obj interface{}, schemaName string) (bool, map[string][]string) {
+	path := "https://raw.githubusercontent.com/BaelorSwift/api/dev/schema/%s.json"
+	schema := goSchema.NewReferenceLoader(fmt.Sprintf(path, schemaName))
+	doc := goSchema.NewGoLoader(obj)
 
 	result, err := goSchema.Validate(schema, doc)
 	if err != nil {
@@ -20,5 +21,11 @@ func Validate(obj interface{}, schemaName string) (bool, []goSchema.ResultError)
 		return true, nil
 	}
 
-	return false, result.Errors()
+	metadata := make(map[string][]string)
+	for _, desc := range result.Errors() {
+		fieldSlice := metadata[desc.Field()]
+		metadata[desc.Field()] = append(fieldSlice, desc.Description())
+	}
+
+	return false, metadata
 }
