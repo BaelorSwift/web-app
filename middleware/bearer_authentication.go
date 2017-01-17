34e8849f528d166c5719151ad5783e1ab681e720
Alex Forbes-Reed <github@alx.red>
2017-01-17 14:01:38 +0000

    wrote auth middleware

---
+++ middleware/bearer_authentication.go
@@ -0,0 +1,42 @@
+package middleware
+
+import (
+	"strings"
+
+	"net/http"
+
+	m "github.com/baelorswift/api/models"
+	"gopkg.in/gin-gonic/gin.v1"
+)
+
+const (
+	// AuthUserKey is the key in the gin context that stores the id of the authenticated user
+	AuthUserKey = "authenticated_user"
+	tokenType   = "bearer "
+)
+
+// BearerAuth ..
+func BearerAuth(context *m.Context) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		err := m.NewBaelorError("invalid_authentication", nil)
+		token := c.Request.Header.Get("Authorization")
+
+		// Check auth starts with 'bearer'
+		tokenLower := strings.ToLower(token)
+		if !strings.HasPrefix(tokenLower, tokenType) {
+			c.JSON(http.StatusUnauthorized, err)
+			c.Abort()
+		}
+
+		// Get api key and compare with database
+		user := &m.User{}
+		query := context.Db.First(user, "api_key = ?", token[len(tokenType):])
+		if query.RecordNotFound() {
+			c.JSON(http.StatusUnauthorized, err)
+			c.Abort()
+		}
+
+		// Set user id to gin context
+		c.Set(AuthUserKey, user.ID)
+	}
+}
