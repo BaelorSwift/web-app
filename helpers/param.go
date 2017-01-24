7760be8a25b53c933a1a8cf51639057951d1e2c7
Alex Forbes-Reed <github@alx.red>
2017-01-24 22:40:55 +0000

    updated how param ident detection works

--- helpers/param.go
+++ helpers/param.go
@@ -1,28 +1,29 @@
 package helpers
 
-import "regexp"
-import "fmt"
-
-const (
-	columnNameFormat = "%s_slug"
-	typeIDKey        = "id"
-	typeSlugKey      = "slug"
+import (
+	"fmt"
+	"regexp"
+	"strings"
 )
 
 // DetectParamType ..
-func DetectParamType(param, columnName string) (string, string) {
-	r := regexp.MustCompile(`(?i)(?P<Type>slug|id)\((?P<Identifier>[a-z0-9\-]+)\)`)
+func DetectParamType(param string, identTypes map[string]string) (string, string) {
+	identTypeKeys := make([]string, len(identTypes))
+	for key := range identTypes {
+		identTypeKeys = append(identTypeKeys, key)
+	}
+
+	r := regexp.MustCompile(fmt.Sprintf(`(?i)(?P<Type>%s)\((?P<Identifier>[a-z0-9\-]+)\)`, strings.Join(identTypeKeys, "|")))
 	matched := r.FindStringSubmatch(param)
 	if len(matched) < 3 {
 		return "id", param
 	}
 
-	switch matched[1] {
-	case typeIDKey:
-		return typeIDKey, matched[2]
-	case typeSlugKey:
-		return fmt.Sprintf(columnNameFormat, columnName), matched[2]
-	default:
-		panic("impossible outcome")
+	for key, value := range identTypes {
+		if matched[1] == key {
+			return value, matched[2]
+		}
 	}
+
+	panic("impossibru outcome")
 }
