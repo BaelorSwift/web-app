759a3efdc9425d1af89a6224e1295f010ad09d7e
Alex Forbes-Reed <github@alx.red>
2017-10-08 19:28:05 +0100

    Added production boot file

---
+++ backend/src/boot/start.js
@@ -0,0 +1,35 @@
+/* eslint-disable no-process-env, no-process-exit, func-style */
+
+import 'babel-polyfill';
+import App from '../app';
+import Server from '../server';
+import log from 'cuvva-log';
+import logSentry from 'cuvva-log-sentry';
+import raven from 'raven';
+import * as Services from '../services';
+
+const defaultPort = 3000;
+const port = process.env.PORT || defaultPort;
+const config = JSON.parse(process.env.CONFIG);
+
+const sentry = new raven.Client(config.sentryDSN);
+
+log.setHandler(logSentry(sentry));
+log.setHandler('fatal', logSentry(sentry, () => process.exit(1)));
+
+const run = async () => {
+	const database = await Services.Database.connect({ uri: 'mongodb://localhost/baelor' });
+	const app = new App(database);
+	const server = new Server(app, { port, ...config });
+
+	await server.setup();
+	server.run();
+};
+
+(async () => {
+	try {
+		await run();
+	} catch (error) {
+		log.fatal('start_failed', [error]);
+	}
+})();
